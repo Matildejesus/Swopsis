4970dd2b670be3a7747171e72b78f402a4b64666
Matildejesus <123233626+Matildejesus@users.noreply.github.com>
2024-07-22 14:09:25 +0800

    map linked to postcode

--- screens/MapsScreen.js
+++ screens/MapsScreen.js
@@ -1,13 +1,38 @@
-import React from "react";
-import MapView from "react-native-maps";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
+import Map from "../Map"; // Assuming Map is the component that handles the map view
+import RegisterContainer from "../components/authentication/RegisterContainer";
+import PrimaryButton from "../components/PrimaryButton";
 
-function MapsScreen({route}) {
-  const { postcode } = route.params;
-  console.log("postcode: " + postcode);
+function MapsScreen({ route }) {
+  const [newPostcode, setNewPostcode] = useState("");
+  const [postcode, setPostcode] = useState(route.params.postcode);
+
+  const handleSearch = () => {
+    if (!newPostcode) return;
+    setPostcode(newPostcode);
+    setNewPostcode(""); // Clear the text input
+    console.log("New postcode set: " + newPostcode); // Debugging log
+  };
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      {/* Map component */}
+      <Map apikey={'FjRYhw4teVr0pkKgzacHIVAyEXKoDe_G4jBNQhILcsQ'} postcode={postcode} />
+      
+      {/* Text input on top of the map */}
+      <View style={styles.inputContainer}>
+        <RegisterContainer
+          placeholder={"Enter your postcode"}
+          onChangeText={setNewPostcode} // This listens for changes in the input field
+          value={newPostcode}
+        />
+        <PrimaryButton 
+          title="Search"
+          style={{ width: 200 }}
+          onPress={handleSearch}
+        />
+      </View>
     </View>
   );
 }
@@ -21,5 +46,14 @@ const styles = StyleSheet.create({
   map: {
     width: "100%",
     height: "100%",
+  },
+  inputContainer: {
+    position: 'absolute',
+    flexDirection: 'row',
+    bottom: 10, // Adjust as needed
+    left: 10, // Adjust as needed
+    right: 10, // Adjust as needed
+    padding: 10,
+    zIndex: 1, // Ensure the input is on top of the map
   },
 });
