67cf96570a3ea214eb168d3c8184bdf9a38a90e4
Matildejesus <123233626+Matildejesus@users.noreply.github.com>
2024-11-24 18:32:52 +0800

    created docker for local environment testing

--- services/supabase.js
+++ services/supabase.js
@@ -1,9 +1,9 @@
 import "react-native-url-polyfill/auto";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ubohapcfavgltukxiirg.supabase.co";
+const supabaseUrl = "http://127.0.0.1:54321";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVib2hhcGNmYXZnbHR1a3hpaXJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk0OTYxMjIsImV4cCI6MjAxNTA3MjEyMn0.x8iemsbERUbX6TP-4Ragqi07-LTXZE-s-bb2PkSfBDc";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
