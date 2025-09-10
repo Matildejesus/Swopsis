b5b58cb25bb70f4c098acb5af3b525598ab33af8
Matildejesus <123233626+Matildejesus@users.noreply.github.com>
2025-09-11 00:59:39 +0800

    fixed take pics

--- components/PicturePicker.js
+++ components/PicturePicker.js
@@ -1,33 +1,31 @@
 import { View, StyleSheet, Image } from "react-native";
 import Colors from "../constants/colors";
 import * as ImagePicker from "expo-image-picker";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ModalPhotoWidget from "./modals/ModalPhotoWidget";
 import PictureButton from "./icons/PictureButton";
 
 function PicturePicker({ userPicture, style, onImageSelected, imageStyle }) {
     const [isModalVisible, setIsModalVisible] = useState(false);
     const [avatar, setAvatar] = useState(userPicture);
-    
+
     const requestPermission = async () => {
-        const { status } =
-            await ImagePicker.requestMediaLibraryPermissionsAsync();
-        if (status !== "granted") {
+        const lib = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        const cam = await ImagePicker.requestCameraPermissionsAsync();
+        if (cam.status !== "granted") {
             alert("Sorry, we need camera roll permissions to make this work.");
             return;
         }
-        const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
-        if (cameraStatus.status !== "granted") {
+        if (lib.status !== "granted") {
             alert("Sorry, we need camera permissions to make this work.");
-        } else {
-            setIsModalVisible(true);
-        }
+        } 
+        setIsModalVisible(true);
     };
 
     const pickImage = async () => {
         // No permissions request is necessary for launching the image library
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.All,
+            mediaTypes: ['images'],
             allowsEditing: true,
             aspect: [4, 3],
             quality: 1,
@@ -41,12 +39,14 @@ function PicturePicker({ userPicture, style, onImageSelected, imageStyle }) {
     };
 
     const takeImage = async () => {
-        let result = await ImagePicker.launchCameraAsync({
-            mediaTypes: ImagePicker.MediaType.Images,
+        console.log("takeImage tapped");
+        let result = await ImagePicker.launchCameraAsync({ 
+            mediaTypes: ['images'],
             allowsEditing: true,
             aspect: [4, 3],
             quality: 1,
         });
+        console.log("takeImage result:", result);
         if (!result.canceled) {
             setAvatar(result.assets[0].uri);
             onImageSelected(result.assets[0].uri);
@@ -61,7 +61,7 @@ function PicturePicker({ userPicture, style, onImageSelected, imageStyle }) {
     return (
         <View>
             {avatar ? (
-                <Image source={{ uri: avatar }} style={imageStyle} />
+                <Image key={avatar} source={{ uri: avatar }} style={imageStyle} />
             ) : (
                 <View style={style ? style : styles.imageContainer} />
             )}
